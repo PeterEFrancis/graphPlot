7a18b60b5aef6e4219b08ea0f5c44dae3e534ab2
Peter Francis <francisp336@gmail.com>
2020-01-26 01:16:11 -0500

    0.0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name="graphPlot",
     packages=["graphPlot"],
-    version="0.0.8",
+    version="0.0.9",
     author="Peter Francis",
     author_email="franpe02@gettysburg.edu",
     description="Plot (di)graphs using a timestep simulation of charged particles and springs",
